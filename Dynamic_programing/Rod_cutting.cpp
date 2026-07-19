8283961f1e54c017b21686da4f22ea7bbe98247d
nikhil-chhillar-2005 <nikhilchhillar957@gmail.com>
2026-07-19 07:37:48 +0000

    unbounded knapsack

---
+++ Dynamic_programing/Rod_cutting.cpp
@@ -0,0 +1,28 @@
+// Given a rod of length n inches and an array price[], where price[i] denotes the value of a piece of length i. Your task is to determine the maximum value obtainable by cutting up the rod and selling the pieces.
+
+// Note: n = size of price, and price[] is 1-indexed array.
+
+#include<bits/stdc++.h>
+using namespace std;
+int dp[1005]
+int fun(int len,vector<int> &price){
+    if(len==0) return 0;
+    if(dp[len]!=-1) return 0;
+    int ans=0;
+    for(int lenc=1;lenc<price.size();lenc++){
+        if(len-lenc>=0){
+              ans=max(ans,fun(len-lenc,price)+price[lenc-1]);
+        }
+    }
+    return ans;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    memset(dp,-1,sizeof(dp));
+    vector<int> a(n);
+    for(int &X:a) cin>>X;
+    cout<<fun(n,a);
+}
