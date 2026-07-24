da012d1e98614c78d3c30a4e66231dd61535af01
nikhil-chhillar-2005 <nikhilchhillar957@gmail.com>
2026-07-24 15:26:24 +0000

    pattern 3

---
+++ Striver sheet/Basic/patterns/pattern3.cpp
@@ -0,0 +1,19 @@
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Your code goes here
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
