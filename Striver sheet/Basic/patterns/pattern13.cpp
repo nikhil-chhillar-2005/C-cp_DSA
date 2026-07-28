0867cedb03f18b82dbee0c693ee138e6c67416bc
nikhil-chhillar-2005 <nikhilchhillar957@gmail.com>
2026-07-28 16:18:04 +0000

    pattern 13

---
+++ Striver sheet/Basic/patterns/pattern13.cpp
@@ -0,0 +1,18 @@
+// 1
+// 23
+// 456
+// 78910
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int p=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<p++<<" ";
+        }
+        cout<<endl;
+    }
+
+}
