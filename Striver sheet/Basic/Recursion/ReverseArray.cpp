6f65dd315876f6d349a48c045d83c9ce1b0517f4
nikhil-chhillar-2005 <nikhilchhillar957@gmail.com>
2026-08-06 21:16:14 +0000

    Reverse an array

---
+++ Striver sheet/Basic/Recursion/ReverseArray.cpp
@@ -0,0 +1,20 @@
+// Reverse an array
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int i=0,j=n-1;
+    while(i<j){
+        swap(a[i],a[j]);
+        i++;
+        j--;
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
