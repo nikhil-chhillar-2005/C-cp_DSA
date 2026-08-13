34ae87d18e6a72a878fc67a302a44579771a5531
nikhil-chhillar-2005 <nikhilchhillar957@gmail.com>
2026-08-13 17:30:16 +0000

    second

--- Striver sheet/Problem on Array/Easy/secondLargest.cpp
+++ Striver sheet/Problem on Array/Easy/secondLargest.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    int l1=-INT_MAX,l2=INT_MAX;
+    if(n<=1){
+        cout<<-1<<endl;
+        return 0;
+    }
+    int l1=-INT_MAX,l2=INT_MAX,s1=INT_MAX,s2=INT_MAX;
     for(int i=0;i<n;i++){
         int x;
         cin>>x;
@@ -15,7 +19,13 @@ int main() {
         }else if(x>l2){
             l2=x;
         }
+        if(x<s1){
+            s2=s1;
+            s1=x;
+        }else if(x<s2){
+            s2=x;
+        }
     }
-    cout<<l2<<endl;
+    cout<<l2<<s2<<endl;
     return 0;
 }
